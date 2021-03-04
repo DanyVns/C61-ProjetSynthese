458a35c7c48d0fa5a26758adef80b2357f50cd43
Dany Viens <dany.viens@gmail.com>
2021-03-04 15:43:29 -0500

    Écran login - prototype. + ajouts de nouveau modules

--- dev/app.js
+++ dev/app.js
@@ -1,8 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require("mongoose");
+var flash = require("connect-flash");
+var passport = require("passport");
+var setUpPassport = require("./setuppassport");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
